1ad78ce5d0232a5b1aa9c61467bd000c5c327ca5
João Marcos <joaomarcosmr4@gmail.com>
2024-04-19 17:58:29 -0300

    .

--- gruppeFood/src/components/Footer/Footer.jsx
+++ gruppeFood/src/components/Footer/Footer.jsx
@@ -4,7 +4,12 @@ import './Footer.css'
 const Footer = () => {
   return (
     <section className='footer'>
-      <p>ooo</p>
+      <h4>
+        GruppeFood
+      </h4>
+    <p>© Copyright 2021 - iFood - Todos os direitos reservados iFood com Agência de Restaurantes Online S.A.
+
+CNPJ 14.380.200/0001-21 / Avenida dos Autonomistas, nº 1496, Vila Yara, Osasco/SP - CEP 06.020-902</p>
     </section>
   )
 }
